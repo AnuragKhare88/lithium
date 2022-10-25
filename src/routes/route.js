2d5d57f5ea62724e867333e0f8e21557b864346b
AnuragKhare88 <anurag.khare35@gmail.com>
2022-10-25 13:42:56 +0530

    mongo post and get API

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+const UserController= require("../controllers/userController");
+const userModel = require('../models/userModel.js');
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createBook", async function ( req, res){
+    let data = req.body
+    let savedData = await userModel.create(data)
+    res.send({savedData})
+})
 
-router.get("/getUsersData", UserController.getUsersData)
+
+router.get("/getAllBooks", async function(req,res){
+    let allBooks = await userModel.find()
+    res.send({msg: allBooks})
+
+
+})
 
 module.exports = router;
