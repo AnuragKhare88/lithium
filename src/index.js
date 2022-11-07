94de10194d5815dc0daaa1debfa8cad3184aefb6
AnuragKhare88 <anurag.khare35@gmail.com>
2022-11-07 12:59:53 +0530

    add auth 2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://anurag:jhansi112233@my-cluster.cummqwt.mongodb.net/test", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,14 +1,35 @@
 const authenticate = function(req, req, next) {
     //check the token in request header
     //validate this token
-
+    let token = req.headers["x-auth-token"];
+    if (!token) return res.send({ status: false, msg: "token must be present" });
+    console.log(token);
+  
+    let decodedToken = jwt.verify(token, "functionup-lithium");
+    if (!decodedToken)
+      return res.send({ status: false, msg: "token is invalid" });
     
+      next ()
+  }
 
-    next()
-}
 
 
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
-}
+    const authorise = function(req, res, next) {
+      // comapre the logged in user's id and the id in request
+     if (user.userId!== userId){
+      return res.send("Invalid Credentials");
+     }
+  
+  
+          if(user.password !== password){
+      return res.send("Invalid Credentials");
+    }
+    
+  
+      next()
+  }
+
+
+
+module.exports.authenticate = authenticate
+module.exports.authorise = authorise
